6f9d7a15652954edf3bdb48b89e3a3d042b04b0c
David Kerr <kerrky@gmail.com>
2024-05-20 23:52:06 -0400

    first push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 className='text text-3xl text-blue-500'>Hello World!</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
